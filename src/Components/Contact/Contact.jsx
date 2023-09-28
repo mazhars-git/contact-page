cc0d08e2e7c799a3579d65d6037018ddd246d5f0
mazhar <mazharulislam611@gmail.com>
2023-09-28 22:44:30 +0600

    update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className='bg-black text-neutral-content'>
-            <div className="grid grid-cols-1 md:grid-cols-2 p-10">
+        <div className='bg-black text-neutral-content py-28'>
+            <div className="grid grid-cols-1 md:grid-cols-2 px-10 pb-2">
                 <div>
                     <h2 className='text-3xl font-semibold'>Contact</h2>
                     <p>732/21 Second Street, Manchester</p>
@@ -11,31 +11,41 @@ const Contact = () => {
                 </div>
                 <div>
                     <h2 className="text-3xl font-semibold">T: +65 (0)76-890-214</h2>
-                    <h2 className="text-3xl font-semibold">E: bookings@wilma.co.uk</h2>                    
+                    <h2 className="text-3xl font-semibold">E: bookings@wilma.co.uk</h2>
                 </div>
             </div>
-            <footer className="footer p-10 text-neutral-content">
-                <nav>
-                    <header className="footer-title">Services</header>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav>
-                    <header className="footer-title">Company</header>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </nav>
-                <nav>
-                    <header className="footer-title">Legal</header>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
-                </nav>
-            </footer>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 px-10 py-5">
+                <div>
+                    <header className="text-3xl font-bold pb-2">Follow Us</header>
+                    <ul>
+                        <li className="link link-hover">Facebook</li>
+                        <li className="link link-hover">Instagram</li>
+                        <li className="link link-hover">TripAdvisor</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2 className="text-3xl font-bold mb-4">Opening Hours</h2>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <div>
+                            <header className="footer-title">Lunch Time</header>
+                            <ul>
+                                <li className="link link-hover">Monday to Sunday</li>
+                                <li className="link link-hover">10.30am-3:00pm</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <header className="footer-title">Dinner Time</header>
+                            <ul>
+                                <li className="link link-hover">Monday to Sunday</li>
+                                <li className="link link-hover">5.30am-11:00pm</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
