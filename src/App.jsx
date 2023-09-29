0ac7df2f5426ff5ddc1c29ea212ed9173189ba78
mazhar <mazharulislam611@gmail.com>
2023-09-29 07:58:20 +0600

    map added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import Contact from './Components/Contact/Contact'
 import ContactBanner from './Components/Contact/ContactBanner'
 import ContactForm from './Components/Contact/ContactForm'
+import MapLocation from './Components/Contact/MapLocation'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="">
       <ContactBanner />
       <Contact />
-      <ContactForm />
+      <MapLocation />
+      <ContactForm />     
     </div>
   )
 }
--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -4,49 +4,48 @@ const ContactForm = () => {
     return (
         <div className="p-10 bg-black text-white">
             <div className="grid grid-cols-1 md:grid-cols-3">
-                <div className="mr-4">
-                    <h1>Get in touch</h1>
-                    <p>
-                        If you have questions or comments, please get a hold of us in whichever way is most convenient. Ask away. There is no reasonable question that our team can not answer. <br />
+                <div className="mr-28 pr-12">
+                    <h1 className='text-5xl font-semibold'>Get in touch</h1>
+                    <p className='text-lg pt-5'>
+                        If you have questions or comments, please get a hold of us in whichever way is most convenient. Ask away. There is no reasonable question that our team can not answer. <br /> <br />
 
                         Craft beer elit seitan exercitation, photo booth et 8-bit kale chips proident chillwave doop vlaborum.
                     </p>
                 </div>
-                <div className='col-span-2 px-8'>
+                <div className='col-span-2 px-10'>
                     <form>
                         <div className='pb-4'>
-                            <label for="name">Your name</label>
+                            <label htmlFor="name">Your name</label>
                             <input type="text" name='name' 
-                                class="form-input 
+                                className="form-input 
                                 px-5 py-3 w-full 
                                 rounded-sm border-2 border-white 
                                 bg-transparent" 
                             />
                         </div>
                         <div className='mb-4'>
-                            <label for="email">Your email</label>
+                            <label htmlFor="email">Your email</label>
                             <input type="email" name='email' 
-                                class="form-input 
+                                className="form-input 
                                 px-5 py-3 w-full 
                                 rounded-sm border-2 border-white 
                                 bg-transparent" 
                             />
                         </div>
                         <div className='pb-3'>
-                            <label for="message">Your message (optional)</label>
+                            <label htmlFor="message">Your message (optional)</label>
                             <textarea 
                                 cols="5" 
                                 rows="3"
                                 name='message' 
-                                class="form-input
+                                className="form-input
                                 px-5 py-3 w-full 
                                 rounded-sm border-2
                                 border-white 
                                 bg-transparent"
                             />
                         </div>
-                        <button className="btn btn-success text-white">Submit</button>
-                       
+                        <button className="btn btn-success px-8 text-white">Submit</button>
                     </form>
                 </div>
             </div>
