94bb9a1561ef5a9472c80d472e4b252171e0670a
mazhar <mazharulislam611@gmail.com>
2023-09-29 09:28:15 +0600

    update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className='bg-black text-neutral-content py-28'>
+        <div className='bg-black text-neutral-content py-28 px-10'>
             <div className="grid grid-cols-1 md:grid-cols-2 px-10 pb-2">
                 <div>
-                    <h2 className='text-3xl font-semibold'>Contact</h2>
+                    <h2 className='text-3xl font-semibold pb-3'>Contact</h2>
                     <p>732/21 Second Street, Manchester</p>
                     <p>Kingston United Kingdom</p>
                 </div>
                 <div>
-                    <h2 className="text-3xl font-semibold">T: +65 (0)76-890-214</h2>
+                    <h2 className="text-3xl font-semibold pb-3">T: +65 (0)76-890-214</h2>
                     <h2 className="text-3xl font-semibold">E: bookings@wilma.co.uk</h2>
                 </div>
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 px-10 py-5">
                 <div>
-                    <header className="text-3xl font-bold pb-2">Follow Us</header>
+                    <h2 className="text-3xl font-bold pb-3">Follow Us</h2>
                     <ul>
                         <li className="link link-hover">Facebook</li>
                         <li className="link link-hover">Instagram</li>
@@ -26,18 +26,18 @@ const Contact = () => {
                 </div>
 
                 <div>
-                    <h2 className="text-3xl font-bold mb-4">Opening Hours</h2>
+                    <h2 className="text-3xl font-bold pb-3">Opening Hours</h2>
 
                     <div className="grid grid-cols-1 md:grid-cols-2">
                         <div>
-                            <header className="footer-title">Lunch Time</header>
+                            <h2 className="text-2xl font-semibold pb-2">Lunch Time</h2>
                             <ul>
                                 <li className="link link-hover">Monday to Sunday</li>
                                 <li className="link link-hover">10.30am-3:00pm</li>
                             </ul>
                         </div>
                         <div>
-                            <header className="footer-title">Dinner Time</header>
+                            <h2 className="text-2xl font-semibold pb-2">Dinner Time</h2>
                             <ul>
                                 <li className="link link-hover">Monday to Sunday</li>
                                 <li className="link link-hover">5.30am-11:00pm</li>
@@ -47,7 +47,6 @@ const Contact = () => {
                 </div>
             </div>
         </div>
-
     );
 };
 
--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -4,48 +4,48 @@ const ContactForm = () => {
     return (
         <div className="p-10 bg-black text-white">
             <div className="grid grid-cols-1 md:grid-cols-3">
-                <div className="mr-28 pr-12">
+                <div className="mr-28 px-12 py-5">
                     <h1 className='text-5xl font-semibold'>Get in touch</h1>
-                    <p className='text-lg pt-5'>
+                    <p className='text-xl pt-5'>
                         If you have questions or comments, please get a hold of us in whichever way is most convenient. Ask away. There is no reasonable question that our team can not answer. <br /> <br />
 
                         Craft beer elit seitan exercitation, photo booth et 8-bit kale chips proident chillwave doop vlaborum.
                     </p>
                 </div>
-                <div className='col-span-2 px-10'>
+                <div className='col-span-2 px-10 py-5'>
                     <form>
                         <div className='pb-4'>
-                            <label htmlFor="name">Your name</label>
+                            <label className="text-2xl font-semibold" htmlFor="name">Your name</label>
                             <input type="text" name='name' 
                                 className="form-input 
-                                px-5 py-3 w-full 
-                                rounded-sm border-2 border-white 
+                                px-5 py-4 w-full 
+                                rounded-lg border-2 border-white 
                                 bg-transparent" 
                             />
                         </div>
                         <div className='mb-4'>
-                            <label htmlFor="email">Your email</label>
+                            <label className="text-2xl font-semibold" htmlFor="email">Your email</label>
                             <input type="email" name='email' 
                                 className="form-input 
-                                px-5 py-3 w-full 
-                                rounded-sm border-2 border-white 
+                                px-5 py-4 w-full 
+                                rounded-lg border-2 border-white 
                                 bg-transparent" 
                             />
                         </div>
                         <div className='pb-3'>
-                            <label htmlFor="message">Your message (optional)</label>
+                            <label className="text-2xl font-semibold" htmlFor="message">Your message (optional)</label>
                             <textarea 
                                 cols="5" 
                                 rows="3"
                                 name='message' 
                                 className="form-input
-                                px-5 py-3 w-full 
-                                rounded-sm border-2
+                                px-5 py-4 w-full 
+                                rounded-lg border-2
                                 border-white 
                                 bg-transparent"
                             />
                         </div>
-                        <button className="btn btn-success px-8 text-white">Submit</button>
+                        <button className="btn bg-amber-500 hover:text-amber-500 px-8 text-white">Submit</button>
                     </form>
                 </div>
             </div>
