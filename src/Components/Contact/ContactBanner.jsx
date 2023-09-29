23b3ecb6980363362d045f0d3c04e848ad758deb
mazhar <mazharulislam611@gmail.com>
2023-09-29 08:43:45 +0600

    done

--- src/Components/Contact/ContactBanner.jsx
+++ src/Components/Contact/ContactBanner.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import bgImg from './../../../public/media/banner-1.jpg';
 const ContactBanner = () => {
     return (
-        <div className='text-center py-16'>
-            <p className="text-2xl indent-">FIND US</p>
-            <h1 className="text-6xl font-bold">Contact</h1>
+        <div className="hero min-h-screen" style={{backgroundImage: `url(${bgImg})`}}>
+            <div className='text-center py-28 text-white'>
+                <p className="text-2xl indent-">FIND US</p>
+                <h1 className="text-8xl font-bold">Contact</h1>
+            </div>
         </div>
     );
 };
